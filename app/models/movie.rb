cd1f754bf820c0ab9a2967f5c1285d7b021418a2
Arnie <arniekidd@gmail.com>
2020-07-23 16:27:18 -0500

    submission for deadline

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,29 @@ class Movie
     @@all
   end
 
+  def movie_reviews
+    Review.all.select do |review|
+      review.movie==self
+    end
+  end
+
+  def movie_viewers
+    movie_reviews.map do |review|
+      review.viewer
+    end
+  end
+
+  def ratings
+    self.movie_reviews.map do |review|
+      review.rating
+    end
+  end
+
+  def average_rating
+    self.ratings.sum / self.ratings.count.to_f
+  end
+
+  def self.highest_rated
+  end
+
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,33 @@ class Viewer
   def self.all
     @@all
   end
+
+  def viewer_reviews
+    Review.all.select do |review|
+      review.viewer==self
+    end
+  end
+
+  def reviewed_movies
+    viewer_reviews.map do |review|
+      review.movie
+    end
+  end
+
+  def reviewed_movie?(movie)
+    Review.all.any? do |review|
+      review.viewer == self && review.movie==movie
+    end
+  end
+
+  def viewed_movie(movie, rating)
+    Review.all.select do |review|
+      if review.viewer !=self && review.movie==movie
+        Review.new(self, movie, rating)
+      elsif review.viewer ==self && review.movie == movie
+        review.rating=rating
+      end
+    end
+  end
   
 end
